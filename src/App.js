a38bea7242383ab697ddc4e4dc04f6f43820b88c
Shweta Srivastava <81436968+Shweta2254@users.noreply.github.com>
2021-09-07 06:39:34 +0530

    result function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   
  
 
-  function handleClick(event) {
+  function operateResult(event) {
     setResult(result.concat(event.target.name));
   }
 
@@ -35,46 +35,46 @@ function App() {
         <button id="clear" className = "btn" onClick={clear}>
         clear
         </button>
-        <button name="1" className = "btn" onClick={handleClick}>
+        <button name="1" className = "btn" onClick={operateResult}>
           1
         </button>
-        <button name="2" className = "btn" onClick={handleClick}>
+        <button name="2" className = "btn" onClick={operateResult}>
           2
         </button>
-        <button name="3" className = "btn" onClick={handleClick}>
+        <button name="3" className = "btn" onClick={operateResult}>
           3
         </button>
-        <button name="+" className = "btn" onClick={handleClick}>
+        <button name="+" className = "btn" onClick={operateResult}>
           +
         </button>
-        <button name="4" className = "btn" onClick={handleClick}>
+        <button name="4" className = "btn" onClick={operateResult}>
           4
         </button>
-        <button name="5" className = "btn" onClick={handleClick}>
+        <button name="5" className = "btn" onClick={operateResult}>
           5
         </button>
-        <button name="6" className = "btn" onClick={handleClick}>
+        <button name="6" className = "btn" onClick={operateResult}>
           6
         </button>
-        <button name="-" className = "btn" onClick={handleClick}>
+        <button name="-" className = "btn" onClick={operateResult}>
           -
         </button>
-        <button name="7" className = "btn" onClick={handleClick}>
+        <button name="7" className = "btn" onClick={operateResult}>
           7
         </button>
-        <button name="8" className = "btn" onClick={handleClick}>
+        <button name="8" className = "btn" onClick={operateResult}>
           8
         </button>
-        <button name="9" className = "btn" onClick={handleClick}>
+        <button name="9" className = "btn" onClick={operateResult}>
           9
         </button>
-        <button name="*" className = "btn" onClick={handleClick}>
+        <button name="*" className = "btn" onClick={operateResult}>
           *
         </button>
-        <button name="/" className = "btn" onClick={handleClick}>
+        <button name="/" className = "btn" onClick={operateResult}>
           /
         </button>
-        <button name="0" className = "btn" onClick={handleClick}>
+        <button name="0" className = "btn" onClick={operateResult}>
           0
         </button>
         
